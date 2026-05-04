acafd9bcec499987843c3972ebeccafd6fe4fe2b
DDurant94 <dannyjdurant@gmail.com>
2026-05-04 16:17:29 -0500

    Fix: Routing

--- obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2266056638c834d3cc0abb8fe1121f465f9478")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd02514ae6224bfd24a3cefb793da231a8a25925")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
